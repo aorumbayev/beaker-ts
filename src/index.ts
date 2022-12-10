a55fdb56be877e72ecdedae55b90f708e6ec63e5
Ben Guidarelli <ben.guidarelli@gmail.com>
2022-12-10 04:35:38 -0500

    tweaks to config to not break installs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from './application_client';
 export * from './generate';
 export * as clients from './clients';
-export * as web from './web';
 export * as sandbox from './sandbox';
--- src/web/index.ts
+++
@@ -1,7 +0,0 @@
-export { Wallet } from './wallets/wallet';
-export {
-  PlaceHolderSigner,
-  SessionWalletManager,
-  ImplementedWallets,
-} from './session_wallet';
-export type { SessionWalletData } from './session_wallet';
